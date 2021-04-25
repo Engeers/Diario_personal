97968f1bb1cf9e25d9387759d9d9182870a4a4df
Engeers <Engeersrossis@gmail.com>
2021-04-24 20:53:41 -0400

    creacion de menus

---
+++ diario_menu.h
@@ -0,0 +1,21 @@
+#ifdef MENU_H
+#define MENU_H
+/**
+ * 
+ * 
+ * @file pro_final.h
+ * @author your name (Engeers O. Rossis)
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-24
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+enum opciones_menu{
+    CREAR_NOTAS = 1,
+    EDITAR_NOTAS,
+    HISTORIAL_NOTAS,
+    SALIR
+}
+#endif /*MENU_H*/
